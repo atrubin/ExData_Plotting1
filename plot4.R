89b0fe92fbe7cba3e917968cd8fd54eea13bdf14
atrubin <alexander.trubin@gmail.com>
2015-01-11 00:25:15 +0500

    Minor corrections.

--- plot4.R
+++ plot4.R
@@ -18,21 +18,27 @@ ReadData <- function(x)
 HHPowConsumption <- ReadData("household_power_consumption.txt")
 
 
-png(filename='plot2.png', width=480, height=480, bg = "transparent")
+png(filename='plot4.png', width=480, height=480, bg = "transparent")
 
 par(mfrow=c(2,2))
 
-with(data, {
-    plot(datetime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
-    plot(datetime, Voltage, ylab="Voltage", type="l")
-    plot(datetime, Sub_metering_1, type="l", col="black",
+with(HHPowConsumption, {
+    plot(TimeStamp, Global_active_power, type = 'l', xlab = "", 
+         ylab = "Global Active Power")
+    
+    plot(TimeStamp, Voltage, ylab="Voltage", type="l", xlab = 'datetime')
+    
+    plot(TimeStamp, Sub_metering_1, type='l', col="black",
          xlab="",ylab="Energy sub metering")
-    lines(datetime, Sub_metering_2, col="red")
-    lines(datetime, Sub_metering_3, col="blue")
+    lines(TimeStamp, Sub_metering_2, col="red")
+    lines(TimeStamp, Sub_metering_3, col="blue")
     legend("topright", bty="n", lty=1,col = c("black","red","blue"), 
-           legend = names(data[,6:8]))
-    plot(datetime, Global_reactive_power, type="l")
+           legend = names(HHPowConsumption[,7:9]))
+    
+    
+    plot(TimeStamp, Global_reactive_power, type='l', xlab = 'datetime')
 })
+
 
 
 dev.off()
